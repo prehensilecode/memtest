787fad2a3e57265c038e6992f9796b785a29ddf7
David Chin <prehensilecode@users.noreply.github.com>
2024-03-08 15:30:55 -0500

    Use gig as input value

--- glom.c
+++ glom.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <time.h>
 
 #define GIG 1073741824l
 
@@ -10,30 +11,43 @@ int main(int argc, char** argv)
     char    mesg[1024];
     size_t  size = 0;
     size_t  i = 0;
+    int     ci = 0;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: glom N\n");
-        fprintf(stderr, "    where N = no. of bytes\n");
+        fprintf(stderr, "    where N = no. of gibibytes\n");
         exit(1);
     }
 
     size = atol(argv[1]);
 
-    printf("Trying to allocate %ld chars (%ld GiB) ...\n", size, size/GIG);
+    /* seed RNG with current time */
+    srand(time(NULL));
+    printf("number = %d\n", rand());
+
+    printf("Trying to allocate %ld chars (%ld GiB) ...\n", size * GIG, size);
     fflush(stdout);
 
     for (i = 0; i < 1024; ++i)
         mesg[i] = '\0';
 
-    a = malloc(size);
+    a = malloc(size * GIG);
     if (a == (char*)NULL) {
         sprintf(mesg, "Cannot allocate %ld chars", size);
         perror(mesg);
     } else {
-        printf("Allocated %ld chars for %ld GiB\n", size, size/GIG);
+        printf("Allocated %ld chars for %ld GiB\n", size*GIG, size);
+        printf("Filling memory ...");
+        for (i = 0; i < size * GIG; ++i) {
+            ci = rand();
+            a[i] = (char)( (float)ci / (float)RAND_MAX * 93. + 1. );
+        }
+        printf(" Done\n");
     }
 
+    printf("Freeing memory ...");
     free(a);
+    printf(" Done\n");
     
     return 0;
 }
